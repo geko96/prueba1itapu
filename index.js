b06e14663558d9bb52d086f5c5185cdcbc0e3297
German Koning <gkoning@gotoitsolutions.com>
2025-11-18 08:32:52 -0300

    creamos el endpoint de registrarse

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/api/v1/auth/register', (req, res) => {
+    res.send('Register');
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
