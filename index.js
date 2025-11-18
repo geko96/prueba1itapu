1c06f21993c4f267546ea32bde63a3706f6a4863
German Koning <gkoning@gotoitsolutions.com>
2025-11-18 08:35:59 -0300

    endpint german

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/api/v1/auth/register', (req, res) => {
     res.send('Register');
 });
 
+app.get('/users/german', (req, res) => {
+    res.send('Login');
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
